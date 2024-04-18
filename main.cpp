7be3a9cb6b125d15ced6c7b15ae9dc6a6830818f
ElRacoon <lea.granier@outlook.fr>
2024-04-18 14:05:58 +0200

    France flag

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int width = 30;
+    int height = 15;
+
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            if (j < width / 3)
+                std::cout << "\033[41m ";
+            else if (j < 2 * width / 3)
+                std::cout << "\033[47m ";
+            else
+                std::cout << "\033[44m ";
+        }
+        std::cout << "\033[0m\n";
+    }
+
     return 0;
 }
